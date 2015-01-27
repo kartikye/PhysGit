bec6219e6e6cd4aa1d9875101e5418d07ccd1630
kartikye <kartikye.mittal@gmail.com>
2015-01-27 19:31:56 +0800

    fian

--- PhysGit.py
+++ PhysGit.py
@@ -3,12 +3,12 @@ from os import system
 import logging
 logging.basicConfig(level=logging.INFO)
 
-import gntp.notifier
+try:
+	import gntp.notifier
+except:
+	pass	
 import sys
 import os
-#sys.path.insert(0, "C:\Users\Kartikye\Downloads\Leap_Motion_SDK_Windows_2.2.2\LeapDeveloperKit_2.2.2+24469_win\LeapSDK\lib")
-#sys.path.insert(0, "/LeapSDK/lib")
-sys.path.insert(0, "LeapSDK")
 import LeapPython
 import Leap
 import time
